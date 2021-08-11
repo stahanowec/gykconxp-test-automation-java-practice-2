ff4e171f125034b51ce59639859171bbc201a39d
API Client <api-client@example.com>
2021-08-11 14:19:40 +0000

    Initial commit

---
+++ src/main/java/com/epam/test/automation/java/practice2/Main.java
@@ -0,0 +1,34 @@
+package com.epam.test.automation.java.practice2;
+
+public class Main {
+
+    /**
+     * <summary>
+     * Implement code according to description of task 1.
+     * </summary>
+     */
+    public static int task1(int value) {
+        //TODO: Delete line below and write your own solution;
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * <summary>
+     * Implement code according to description of task 2.
+     * </summary>
+     */
+    public static int task2(int value) {
+        //TODO: Delete line below and write your own solution;
+        throw new UnsupportedOperationException();
+    }
+
+    /**
+     * <summary>
+     * Implement code according to description of task 3.
+     * </summary>
+     */
+    public static int task3(int value) {
+        //TODO: Delete line below and write your own solution;
+        throw new UnsupportedOperationException();
+    }
+}
